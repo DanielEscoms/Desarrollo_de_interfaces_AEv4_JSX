82b4647b877ba2dc3b912cfc68854094fb00b796
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-03 01:01:07 +0100

    Ejercicio 10

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 
 const nombre = nombreCompleto => {
@@ -25,7 +25,7 @@ const datos = labelText => {
   );
 };
 
-const botonInformes = () =>{
+const botonInformes = () => {
   return (
     <Button
       icon='format-list-bulleted'
@@ -36,7 +36,16 @@ const botonInformes = () =>{
 };
 
 const estilo = 'florida';
-const isAdmin = true;
+const isAdmin = false;
+const modulos2Dam = [
+  {nombre: 'DIN', profesor: 'Manel', horas: 120},
+  {nombre: 'ADA', profesor: 'Juanmi', horas: 120},
+  {nombre: 'PMDM', profesor: 'Fran', horas: 100},
+  {nombre: 'PSP', profesor: 'Fran', horas: 60},
+  {nombre: 'SGE', profesor: 'Belén', horas: 100},
+  {nombre: 'Inglés', profesor: 'Caterina', horas: 40},
+  {nombre: 'EIE', profesor: 'Manuel', horas: 60},
+];
 
 class App extends Component {
   render() {
@@ -45,6 +54,20 @@ class App extends Component {
         {nombre('Daniel Escoms Donate')}
         {datos('Introduce tus datos')}
         {isAdmin ? botonInformes() : null}
+        <ScrollView>
+          {modulos2Dam.map((item, pos) => {
+            return (
+              <View
+                key={item.nombre}
+                style={pos % 2 === 0 ? styles.fuerte : styles.tenue}>
+                <Text>{pos + 1}</Text>
+                <Text>{item.nombre}</Text>
+                <Text>{item.profesor}</Text>
+                <Text>{item.horas} horas</Text>
+              </View>
+            );
+          })}
+        </ScrollView>
       </>
     );
   }
@@ -67,6 +90,12 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  fuerte: {
+    backgroundColor: '#F48FB1',
+  },
+  tenue: {
+    backgroundColor: '#F8BBD0',
   },
 });
 
